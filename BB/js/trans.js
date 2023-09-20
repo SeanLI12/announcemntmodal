669d536e4deeb53ccf21c978fa7713bb0e8de9e4
khsamonte <khsamonte@gmail.com>
2023-09-20 18:36:38 +0800

    Not sure

--- BB/js/trans.js
+++ BB/js/trans.js
@@ -1,161 +1,8 @@
 // The translations for the texts and content in the modal
-const translationData = [
-  {
-    trans_id: "header_text",
-    en: "INTRODUCING BET BUILDER",
-    zh: "隆重推出创造玩法",
-    vi: "GIỚI THIỆU CƯỢC TÙY CHỌN",
-    id: "APA ITU BET BUILDER",
-    th: "ใหม่! สร้างบิลสเต็ปได้ในแมทช์เดียว",
-    ko: "베팅빌더를 소개합니다",
-    pt: "Criar Aposta, introdução!",
-    km: "សេចក្តីណែនាំ​ស្តីពីមុខងារ ការបង្កើតប័ណ្ណភ្នាល់ (Bet Builder)",
-    ja: "ベットビルダーの紹介",
-  },
-  {
-    trans_id: "subheader_text",
-    en: "Available for selected Football Matches",
-    zh: "精选的足球赛事皆有",
-    vi: "Hiện đã khả dụng cho các trận bóng được chọn.",
-    id: "Tersedia untuk Pertandingan Sepak Bola tertentu",
-    th: "เปิดใช้งานแล้วในแมทช์ฟุตบอลที่กำหนด",
-    ko: "선정 축구 경기에서 이용가능합니다.",
-    pt: "Disponível em jogos selecionados",
-    km: "មានលើការប្រកួតបាល់ទាត់ដែលបានជ្រើសរើស",
-    ja: "特定のサッカーの試合でご利用いただけます",
-  },
-  {
-    trans_id: "bet_builder_text",
-    en: [
-      "Bet Builder is a type of bet where you can combine selections from the same event into a single bet – a same-event parlay or multiple!",
-    ],
-    zh: [
-      "创造玩法是一种可以让您在同一场赛事中自行组合盘口成为一个注单 -- 犹如同场赛事的连串过关或复式过关!",
-    ],
-    vi: [
-      "Cược Tùy Chọn là một loại cược mà Quý Khách có thể kết hợp các lựa chọn từ cùng một trận đấu thành một cược đơn hoặc một cược xâu từ cùng trận đấu!",
-    ],
-    id: [
-      "Bet Builder adalah jenis taruhan di mana Anda dapat menggabungkan pilihan dari pertandingan yang sama menjadi satu taruhan – pertandingan parlay yang sama atau beberapa!",
-    ],
-    th: [
-      '"สร้างบิลสเต็ป" ฟีเจอร์ใหม่ที่คุณสามารถเลือกเดิมพันได้หลายประเภทพร้อมกันในแมทช์เดียว',
-    ],
-    ko: [
-      "동일 경기에서 여러 항목을 선택해 하나의 베팅으로 조합할 수 있는 기능입니다. 팔레이 또는 조합 베팅과 유사합니다.",
-    ],
-    pt: [
-      "A função Criar Aposta é um recurso que permite você criar uma aposta múltipla no mesmo evento, combinando diversos mercados do mesmo jogo em uma única aposta.",
-    ],
-    km: [
-      "ការបង្កើតប័ណ្ណភ្នាល់ គឺ​ជា​ប្រភេទ​នៃ​ការ​ភ្នាល់មួយ ដែលអ្នក​អាចច្របាច់​បញ្ចូល​គ្នា​រេវាង​ជម្រើស​​នានា ពី​ព្រឹត្តិ​ការណ៍​តែមួយទៅ​ក្នុង​ប័ណ្ណភ្នាល់ទោល - ដូច​ទៅនឹង​ការ​ភ្នាល់​គូ​បក ឬ ​ការ​ភ្នាល់​គូ​ចម្រុះ!",
-    ],
-    ja: [
-      "ベットビルダーは、同じイベントのセレクションから複数を1つのベットに組み合わせることができるベットの種類です。（同じイベントのパーレイまたはマルチプル）",
-    ],
-  },
-  {
-    trans_id: "learn-more-link",
-    en: "Learn More",
-    zh: "查看更多",
-    vi: "TÌM HIỂU THÊM",
-    id: "PELAJARI LEBIH LANJUT",
-    th: "อ่านเพิ่มเติม",
-    ko: "더 알아보기",
-    pt: "SAIBA MAIS",
-    km: "មើល​បន្ថែមទៀត",
-    ja: "詳細を見る",
-  },
-];
+const translationData = [];
 
 // The translations located inside the animation clip
-const animationTranslationData = [
-  {
-    layerName: "#SelectAMatch",
-    en: "Select a match and look for the Bet Builder tab.",
-    zh: "选一场赛事后寻找‘创造玩法’的选项。",
-    vi: "Chọn 1 trận đấu và tìm kiếm tab Cược Tùy Chọn.",
-    id: "Pilih pertandingan dan cari tab Bet Builder.",
-    th: "เพียงเลือกแมทช์ที่ต้องการและกดปุ่ม สร้างบิลสเต็ป",
-    ko: "경기를 선택하고 베팅빌더 탭을 확인하세요. ",
-    pt: "Selecione um evento e procure a opção ‘Criar Aposta’.",
-    km: "ជ្រើស​រើស​ការ​ប្រកួត​មួយ និង​សម្លឹង​រកប្រអប់ ការបង្កើតប័ណ្ណភ្នាល់",
-    ja: "試合を選択し、ベットビルダータブを探します。",
-  },
-  {
-    layerName: "#ChooseSelections",
-    en: "Choose the selections you want to combine, add to your Bet Slip and Place Bet.",
-    zh: "透过选择建立您的专属\r组合，将其加入您的下注单然后下注。",
-    vi: "Thêm các lựa chọn Quý Khách muốn kết hợp vào Vé Cược và Đặt Cược",
-    id: "Pilih pilihan yang ingin Anda gabungkan, tambahkan ke Slip Taruhan dan Pasang Taruhan.",
-    th: "เลือกเดิมพันที่คุณต้องการสร้างบิลสเต็ป เพิ่มไปที่ สลิปการเดิมพัน เพื่อ วางเดิมพัน",
-    ko: "조합할 항목을 선택하고 베팅 슬립에 추가하여 베팅을 진행하세요.",
-    pt: "Escolha as seleções que deseja combinar, adicione ao seu Boletim de Apostas e clique em Apostar.",
-    km: "ជ្រើសរើសជម្រើសដែលអ្នកចង់បញ្ចូលគ្នា រួចបន្ថែមទៅក្នុងប័ណ្ណភ្នាល់របស់អ្នក និងដាក់ភ្នាល់",
-    ja: "組み合わせたいセレクションを選択しベット伝票に追加後、ベットするを押してください。",
-  },
-  {
-    layerName: "#BetBuilder",
-    en: "Bet Builder",
-    zh: "创造玩法",
-    vi: "Cược Tùy Chọn",
-    id: "Bet Builder",
-    th: "สร้างบิลสเต็ป",
-    ko: "베팅빌더",
-    pt: "Criar Aposta",
-    km: "ការបង្កើតប័ណ្ណភ្នាល់",
-    ja: "ベットビルダー",
-  },
-  {
-    layerName: "#Handicap",
-    en: "Handicap -0.5",
-    zh: "让球 -0.5",
-    vi: "Cược Chấp -0.5",
-    id: "Handicap -0.5",
-    th: "แฮนดิแคป -0.5",
-    ko: "핸디캡 -0.5",
-    pt: "Handicap -0.5",
-    km: "គ្រាប់ត -0.5",
-    ja: "ハンデ -0.5",
-  },
-  {
-    layerName: "#GoalsOver",
-    en: "Goals: Over 3.5",
-    zh: "进球：大 3.5",
-    vi: "Bàn Thắng: Trên 3.5",
-    id: "Gol: Atas 3.5",
-    th: "ประตูรวม: สูง 3.5",
-    ko: "골수 : 오버 3.5",
-    pt: "Gols: Acima 3.5",
-    km: "គ្រាប់បាល់ : លើស 3.5",
-    ja: "ゴール: オーバー 3.5",
-  },
-  {
-    layerName: "#AnytimeGoalScorer",
-    en: "Anytime Goalscorer",
-    zh: "任何时间进球球员",
-    vi: "Cầu Thủ Ghi Bàn",
-    id: "Pencetak Gol Kapan Saja",
-    th: "ผู้ทำประตูเวลาใดก็ได้",
-    ko: "경기 내 득점",
-    pt: "Anytime Goalscorer",
-    km: "អ្នករកគ្រាប់បាល់ពេលណាមួយ",
-    ja: "得点者",
-  },
-  {
-    layerName: "#StartBuilding",
-    en: "Start Building your Bets today.",
-    zh: "今天就开始創建您的专属下注组合。",
-    vi: "Sử dụng Cược Tùy Chọn ngay hôm nay",
-    id: "Pasang Taruhan Anda hari ini.",
-    th: "สร้างบิลสเต็ป ได้แล้ววันนี้",
-    ko: "지금 바로 나만의 베팅을 설정하세요.",
-    pt: "Comece a Criar suas Apostas hoje.",
-    km: "ចាប់ផ្តើមបង្កើតប័ណ្ណភ្នាល់របស់អ្នកថ្ងៃនេះ",
-    // km: '<text x="20" y="35" class="small">My</text>',
-    ja: "今から自分だけのカスタマイズとベット体験を開始。",
-  },
-];
+const animationTranslationData = [];
 
 // Elements
 const animationContainer = document.getElementById("animation_container");
@@ -178,44 +25,97 @@ let lottieJSON = "../../js/animation.json";
 replayButton.style.display = "none";
 // replayOverlay.style.display = "none";
 
-// Generates the translated data in the modal content
-translationData.forEach((objData) => {
-  const $objectEl = document.getElementById(objData.trans_id);
+function fetchTranslationDataFromSheets() {
+  const sheetsData =
+    "https://sheets.googleapis.com/v4/spreadsheets/1CRNEc0zDsAmtAT8iHtSwAhVxinID5202OIFx6-_kmCo/values/sheet1?alt=json&key=AIzaSyDxZruTkrDTIRgPOmhUZtEBQRARBosTGyA";
 
-  if ($objectEl) {
-    if (typeof objData[lang] === "object") {
-      // FOR PARAGRAPHS
-      objData[lang].forEach((textData) => {
-        const $newParaEl = document.createElement("p");
-        $newParaEl.innerHTML = textData;
-        $objectEl.appendChild($newParaEl);
-      });
-    } else if (typeof objData[lang] === "string") {
-      // FOR SINGLE-LINE TEXTS
-      $objectEl.innerHTML = objData[lang];
+  // Fetching the JSON data for translation
+  fetch(sheetsData).then(async (res) => {
+    try {
+      const jsonData = await res.json();
+      convertDatasetForTranslation(jsonData);
+    } catch (error) {
+      console.log("Error in fetching the translation data from Google Sheets.");
     }
-  }
-});
+  });
+}
+
+/**
+ * Converts the data retrieved from Google Sheets to objects-in-array form.
+ *  */
+function convertDatasetForTranslation(data) {
+  const dataSet = data.values;
+
+  dataSet.forEach((translationArray, idx) => {
+    let hasHashTag = false;
+
+    // The keys are in the first index of the dataset, so they're not included
+    if (idx !== 0) {
+      const keys = dataSet[0];
+      const translationObjectToPush = {};
+
+      // Goes through the array to add the values to the object
+      for (let i = 0; i < translationArray.length; i++) {
+        // Checks if the ID value has hashtag (to distinguish translations)
+        if (translationArray[0].charAt(0) === "#") {
+          hasHashTag = true;
+        }
+
+        // Pushes the value to the object with the respective translation key
+        translationObjectToPush[keys[i]] = translationArray[i];
+      }
+
+      // The array to push the object on will depend on the data
+      if (hasHashTag) {
+        animationTranslationData.push(translationObjectToPush);
+      } else {
+        translationData.push(translationObjectToPush);
+      }
+    }
+  });
+
+  generateTranslationDataInModal();
+  fetchJSONFromLottieFile();
+}
 
 // Fetches the JSON data for the localization inside the animation
-// fetch(lottieJSON).then(async (response) => {
-//   try {
-//     const jsonData = await response.json();
-//     loadLottiePlayer(jsonData);
-//   } catch (error) {
-//     console.log("Error happened here!");
-//     console.error(error);
-//   }
-// });
+function fetchJSONFromLottieFile() {
+  fetch(lottieJSON).then(async (res) => {
+    try {
+      const jsonData = await res.json();
+      loadLottiePlayer(jsonData);
+    } catch (error) {
+      console.log("Error in fetching the JSON file to play in Lottie Player.");
+    }
+  });
+}
 
-fetch(lottieJSON).then(async (res) => {
-  try {
-    const jsonData = await res.json();
-    loadLottiePlayer(jsonData);
-  } catch (error) {
-    console.log("Error happened here.");
-  }
-});
+// Generates the translated data in the modal content
+function generateTranslationDataInModal() {
+  translationData.forEach((objData) => {
+    const $objectEl = document.getElementById(objData.key);
+
+    if ($objectEl) {
+      if (typeof objData[lang] === "object") {
+        // FOR PARAGRAPHS
+        objData[lang].forEach((textData) => {
+          const $newParaEl = document.createElement("p");
+          $newParaEl.innerHTML = textData;
+          $objectEl.appendChild($newParaEl);
+        });
+      } else if (typeof objData[lang] === "string") {
+        if (objData.key === "bet_builder_text") {
+          const $newParaEl = document.createElement("p");
+          $newParaEl.innerHTML = objData[lang];
+          $objectEl.appendChild($newParaEl);
+        } else {
+          // FOR SINGLE-LINE TEXTS
+          $objectEl.innerHTML = objData[lang];
+        }
+      }
+    }
+  });
+}
 
 // Dynamically changes the localization inside the animation before it's loaded
 function loadLottiePlayer(lottieData) {
@@ -237,7 +137,7 @@ function loadLottiePlayer(lottieData) {
 
     // Loops through the localisation object and checks matches with the texts
     animationTranslationData.forEach((transData) => {
-      if (lottieData.layers[index].nm === transData.layerName) {
+      if (lottieData.layers[index].nm === transData.key) {
         /**
          * Certain language scripts don't work on certain fonts. This piece of
          * code addresses that problem by substituting a different font.
@@ -250,7 +150,6 @@ function loadLottiePlayer(lottieData) {
         }
 
         // Modifies the text in the data fetched from the JSON
-
         lottieData.layers[index].t.d.k[0].s.t = transData[lang];
       }
     });
@@ -371,3 +270,5 @@ function convertKhmer() {
         <text x="-55" y="20">ស់អ្នកថ្ងៃនេះ</text>`;
   });
 }
+
+fetchTranslationDataFromSheets();
